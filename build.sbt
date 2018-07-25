5d5cce33b9f2b7ad71ecd75ebbbb5650be684611
tz70s <su3g4284zo6y7@gmail.com>
2018-07-25 20:52:38 +0800

    Scaffold practice project layout.

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "clustersingletonex"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
+
+val akkaVersion = "2.5.14"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion
+)
---
+++ src/main/scala/clustersingletonex/Cluster.scala
@@ -0,0 +1,38 @@
+package clustersingletonex
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, PoisonPill, Props}
+import akka.cluster.singleton.{
+  ClusterSingletonManager,
+  ClusterSingletonManagerSettings
+}
+import clustersingletonex.Controller.CallOnce
+
+object Controller {
+  case object CallOnce
+
+  def props = Props(new Controller)
+}
+
+class Controller extends Actor with ActorLogging {
+  import Controller._
+
+  override def receive: Receive = {
+    case CallOnce => println(s"controller call once, from ${sender()}")
+  }
+}
+
+object Cluster {
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("exsystem")
+
+    val controllerRef = system.actorOf(
+      ClusterSingletonManager.props(
+        singletonProps = Controller.props,
+        terminationMessage = PoisonPill,
+        settings = ClusterSingletonManagerSettings(system)
+      )
+    )
+
+    controllerRef ! CallOnce
+  }
+}
